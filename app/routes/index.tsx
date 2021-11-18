32cdeccb4f97c998202706e2507e3dbc460bf6c2
Austin Crim <aust.crim@gmail.com>
2021-11-17 22:07:00 -0600

    tweakin

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -14,15 +14,37 @@ export let loader = async () => {
 
 export default function Index() {
   let count = useLoaderData<number>()
+
+  let message = (function () {
+    if (count === 0) {
+      return "You haven't written any notes yet, what's stopping you?"
+    } else if (count === 1) {
+      return "You've only written 1 note, but that's ok"
+    } else {
+      return `You've written ${count} notes. Nice work.`
+    }
+  })()
+
+  let cta = (function () {
+    if (count === 0) {
+      return (
+        <p className="mt-10">
+          <Link to="/compose">Let's get started</Link>
+        </p>
+      )
+    } else {
+      return (
+        <p className="mt-10">
+          Want to <Link to="/compose">keep going</Link> or{' '}
+          <Link to="/notes">do some review?</Link>
+        </p>
+      )
+    }
+  })()
   return (
     <main>
-      <h1 className="mt-10 text-5xl font-semibold">
-        Hey! You've written {count} notes. Nice work.
-      </h1>
-      <p className="mt-10">
-        Want to <Link to="/compose">keep going</Link> or{' '}
-        <Link to="/notes">do some review?</Link>
-      </p>
+      <h1 className="mt-10 text-5xl font-semibold">Hey! {message}</h1>
+      {cta}
     </main>
   )
 }
